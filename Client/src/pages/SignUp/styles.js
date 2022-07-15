411de7fb71edbc981aff1f984f7fea7cac8e7a18
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-15 14:21:47 -0300

    adding SignUp component

---
+++ Client/src/pages/SignUp/styles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 50vw;
+    }
+
+    div{
+        display: flex;
+        flex-direction: column;
+    }
+
+    input{
+        margin-bottom: 13px;
+        width: 350px;
+        height: 25px;
+        border-radius: 4px;
+        border: 1px solid ;
+    }
+
+    button{
+        width: 120px;
+        padding: 5px;
+        margin-top: 20px;
+    }
+
+    
+
+`
