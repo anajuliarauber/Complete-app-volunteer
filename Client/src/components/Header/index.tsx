9dc784482c7c217a62ffb36728e65fc089b8c704
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-20 14:14:44 -0300

    Creating header

---
+++ Client/src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "./styles";
+
+export function Header () {
+    return(
+        <HeaderContainer>
+            <p>Eu Voluntário</p>
+        </HeaderContainer>
+    )
+}
---
+++ Client/src/components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    width: 100vw;
+    height: 25vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`
