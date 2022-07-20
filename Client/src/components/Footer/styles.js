67c9cea8dd936dc9df61162fc4e263647c143045
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-20 14:14:56 -0300

    creating footer

---
+++ Client/src/components/Footer/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    height: 15vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
