3cf6c4f9365737219ef4f53407cbfe14f5224ac2
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-20 14:15:42 -0300

    organizing folder structure

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,18 @@
+import { Home } from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SignUpPage } from './pages/SignUpPage';
+import { SignInPage } from './pages/SignInPage';
+import { FindVolunteerPage } from './pages/FindVolunteerPage';
 
-function App() {
-  return (
-    <div className="App">
-      APP
-    </div>
-  );
+export function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/signup' element={<SignUpPage />} />
+                <Route path='/signin' element={<SignInPage />} />
+                <Route path='/findvolunteer' element={<FindVolunteerPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App;
