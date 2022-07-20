af025d1c477924be74b258d5fd8ec58f8927300b
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-20 14:16:05 -0300

    creating homepage

---
+++ Client/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { MainHome } from "../../components/MainHome";
+
+export function Home() {
+    return (
+      <>
+        <Header/>
+        <MainHome/>
+        <Footer/>
+      </>
+    );
+  }
+  
+  
