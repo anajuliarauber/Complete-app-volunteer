d2b1878c47cf95e2fcf103d5b618a3321705fe2d
anajuliarauber <anajuliarauber@hotmail.com>
2022-07-20 14:16:23 -0300

    creating signInPage

---
+++ Client/src/pages/SignInPage/index.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { SignIn } from "../../components/SignIn";
+
+export function SignInPage() {
+    
+
+    return (
+        <>
+            <Header />
+            <SignIn/>
+            <Footer />
+        </>
+    )
+}
